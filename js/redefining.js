9439b60ab242afdecf6eaf034f70a35a5dba94b0
zodwo <senanm260@gmail.com>
2023-05-17 01:53:56 +0400

    LookScout

---
+++ js/redefining.js
@@ -0,0 +1,61 @@
+const header = document.querySelector("#head");
+const headerList = document.querySelector(".drop-down-menu");
+const darkLOGO = document.querySelectorAll(".dark");
+const whiteLOGO = document.querySelectorAll(".white");
+const menuColor = document.querySelector(".menu-Wrapper");
+const SignColor = document.querySelector("#sign");
+
+window.addEventListener("scroll", checkscrool);
+
+checkscrool();
+
+function checkscrool() {
+  const main_scrol = 710;
+  const trigger_scrool = document.documentElement.scrollTop;
+
+  if (trigger_scrool >= main_scrol) {
+    hamburger.style.color = "#000";
+    header.style.borderColor = "#eaebf0";
+    header.style.backgroundColor = "#f8f9fb";
+    headerList.style.backgroundColor = "#f8f9fb";
+    menuColor.classList.remove("color-active");
+    SignColor.style.color = "#437EF7";
+
+    darkLOGO.forEach((item) => {
+      item.style.display = "block";
+    });
+
+    whiteLOGO.forEach((item) => {
+      item.style.display = "none";
+    });
+  } else {
+    hamburger.style.color = "#fff";
+    header.style.borderColor = "#648ef7";
+    header.style.backgroundColor = "#2b63d9";
+    headerList.style.backgroundColor = "#2b63d9";
+    menuColor.classList.add("color-active");
+    SignColor.style.color = "#fff";
+
+    darkLOGO.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    whiteLOGO.forEach((item) => {
+      item.style.display = "block";
+    });
+  }
+}
+
+const rightWidth = document.querySelector(".page-1 .right");
+
+window.addEventListener("resize", () => {
+  const mainWidth = 1400;
+  const thisWidth = window.innerWidth;
+  console.log(thisWidth);
+
+  if (thisWidth <= mainWidth) {
+    rightWidth.classList.add("width-sensor")
+  }else{
+    rightWidth.classList.remove("width-sensor")
+  }
+});
